b8faf7d9a5c8c2538e684756aa69fcfe6e24855e
kyryaga <money230704@gmail.com>
2023-04-21 18:06:52 +0300

    refactoring2

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "libs/data_structures/vector/vector.h"
+#include "libs\data_structures\vector\vector.h"
+
+void test() {
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+    test_atVector_notEmptyVector();
+    test_atVector_requestToLastElement();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
+}
 
 int main() {
-    Vector v = createVector(SIZE_MAX);
+    test();
 
     return 0;
 }
