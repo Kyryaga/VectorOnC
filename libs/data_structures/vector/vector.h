1ffb44de5d1e3e38ee2453b1f16ef19411f9b8a5
kyryaga <money230704@gmail.com>
2023-04-21 18:00:38 +0300

    append push / pop functions

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <stdbool.h>
+#include <assert.h>
 
 typedef struct Vector {
     int *data; // указатель на элементы вектора
@@ -17,3 +19,19 @@ void clear(Vector *v);
 void shrinkToFit(Vector *v);
 
 void deleteVector(Vector *v);
+
+bool isEmpty(Vector *v);
+
+bool isFull(Vector *v);
+
+int getVectorValue(Vector *v, size_t i);
+
+void pushBack(Vector *v, int x);
+
+void test_pushBack_emptyVector();
+
+void test_pushBack_fullVector();
+
+void popBack(Vector *v);
+
+void test_popBack_notEmptyVector();
